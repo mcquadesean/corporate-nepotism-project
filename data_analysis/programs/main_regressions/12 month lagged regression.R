e1ee84ea0a7bcc5cb4d1fd36b47deedc5573a397
Sean McQuade <41092833+mcquadesean@users.noreply.github.com>
2023-06-30 18:19:06 -0500

    Update 12 month lagged regression.R

--- data_analysis/programs/main_regressions/12 month lagged regression.R
+++ data_analysis/programs/main_regressions/12 month lagged regression.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(lmtest)
 library(readxl)
 
-data <- read_excel("C:/Users/user/Documents/Honors Thesis/regressions/12 month lag/thesis_reg_12.xlsx")
+data <- read_excel(".../thesis_reg_12.xlsx")
 
 # interaction var
 data$treat_time <- data$treatment * data$time
