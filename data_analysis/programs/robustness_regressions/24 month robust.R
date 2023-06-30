5282cbc4df1c6a8fee143676849cdb77cd64b161
Sean McQuade <41092833+mcquadesean@users.noreply.github.com>
2023-06-30 18:25:19 -0500

    Update 24 month robust.R

--- data_analysis/programs/robustness_regressions/24 month robust.R
+++ data_analysis/programs/robustness_regressions/24 month robust.R
@@ -2,7 +2,7 @@ library(stargazer)
 library(dplyr)
 library(lmtest)
 library(readxl)
-data <- read_excel("C:/Users/user/Documents/Honors Thesis/regressions/robustness regs/24/24_month_lag_robust.xlsx")
+data <- read_excel(".../24_month_lag_robust.xlsx")
 
 # interaction var
 data$treat_time <- data$treatment * data$time
